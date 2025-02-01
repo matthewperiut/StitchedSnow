c126906d8be402cde6759094514b3911aeb69cbc
solonovamax <solonovamax@12oclockpoint.com>
2025-02-01 13:42:46 -0500

    Actually update to 1.21.4 (for real this time), bump version

--- src/main/java/io/github/strikerrocker/mixin/ServerWorldMixin.java
+++ src/main/java/io/github/strikerrocker/mixin/ServerWorldMixin.java
@@ -11,7 +11,6 @@ import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
@@ -26,8 +25,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.function.Supplier;
-
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin extends World {
@@ -36,13 +33,12 @@ public abstract class ServerWorldMixin extends World {
             RegistryKey<World> registryRef,
             DynamicRegistryManager registryManager,
             RegistryEntry<DimensionType> dimensionEntry,
-            Supplier<Profiler> profiler,
             boolean isClient,
             boolean debugWorld,
             long seed,
             int maxChainedNeighborUpdates
     ) {
-        super(properties, registryRef, registryManager, dimensionEntry, profiler, isClient, debugWorld, seed, maxChainedNeighborUpdates);
+        super(properties, registryRef, registryManager, dimensionEntry, isClient, debugWorld, seed, maxChainedNeighborUpdates);
     }
 
     @Inject(
